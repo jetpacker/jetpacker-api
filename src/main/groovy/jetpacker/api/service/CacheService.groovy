fec559f20cea929c5db1294c90fca2793ee22556
forseti <slickwolf@gmail.com>
2016-10-30 20:44:38 +0800

    Add static compilation.

--- src/main/groovy/jetpacker/api/service/CacheService.groovy
+++ src/main/groovy/jetpacker/api/service/CacheService.groovy
@@ -1,10 +1,12 @@
 package jetpacker.api.service
 
+import groovy.transform.CompileStatic
 import org.springframework.stereotype.Service
 
 /**
  * Created by wolf on 30/10/16.
  */
+@CompileStatic
 @Service
 class CacheService {
 }
