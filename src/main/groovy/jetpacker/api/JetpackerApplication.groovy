2ad1e3d429b129000a0b7f1c104e4d91eb7e3a21
forseti <slickwolf@gmail.com>
2016-10-30 20:37:57 +0800

    Enable scheduling. Enable static compilation.

--- src/main/groovy/jetpacker/api/JetpackerApplication.groovy
+++ src/main/groovy/jetpacker/api/JetpackerApplication.groovy
@@ -1,9 +1,13 @@
 package jetpacker.api
 
+import groovy.transform.CompileStatic
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
+@CompileStatic
 class JetpackerApplication {
 	static void main(String[] args) {
 		SpringApplication.run JetpackerApplication, args
