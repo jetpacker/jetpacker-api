1af122908989e331b2287fd6d1450d8dd7a87083
forseti <slickwolf@gmail.com>
2016-10-30 20:40:38 +0800

    Enable static compilation. Add @Bean StreamingTemplateEngine Add @Bean AsyncRestTemplate

--- src/main/groovy/jetpacker/api/configuration/JetpackerConfiguration.groovy
+++ src/main/groovy/jetpacker/api/configuration/JetpackerConfiguration.groovy
@@ -1,12 +1,27 @@
 package jetpacker.api.configuration
 
+import groovy.text.StreamingTemplateEngine
+import groovy.text.TemplateEngine
+import groovy.transform.CompileStatic
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.AsyncRestTemplate
 
 /**
  * Created by wolf on 30/10/16.
  */
 @Configuration
 @EnableConfigurationProperties(JetpackerProperties.class)
+@CompileStatic
 class JetpackerConfiguration {
+    @Bean
+    AsyncRestTemplate asyncRestTemplate() {
+        new AsyncRestTemplate()
+    }
+
+    @Bean
+    TemplateEngine templateEngine() {
+        new StreamingTemplateEngine()
+    }
 }
