3081ea61446c47e088a0f7a9af8615edc78e9478
forseti <slickwolf@gmail.com>
2016-11-02 17:00:13 +0800

    Add @ToString and @CompileStatic

--- src/main/groovy/jetpacker/api/configuration/Releases.groovy
+++ src/main/groovy/jetpacker/api/configuration/Releases.groovy
@@ -1,8 +1,13 @@
 package jetpacker.api.configuration
 
+import groovy.transform.CompileStatic
+import groovy.transform.ToString
+
 /**
  * Created by donny on 30/10/16.
  */
+@ToString(includeNames = true)
+@CompileStatic
 class Releases {
     String defaultVersion
     List<String> versions
