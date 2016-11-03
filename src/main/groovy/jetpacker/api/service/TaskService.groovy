d1ed6af874aacdcb29b570d25b33de278527f568
forseti <slickwolf@gmail.com>
2016-11-04 00:07:57 +0800

    Add codes to retrieve SDKMAN! candidates correctly

--- src/main/groovy/jetpacker/api/service/TaskService.groovy
+++ src/main/groovy/jetpacker/api/service/TaskService.groovy
@@ -1,10 +1,12 @@
 package jetpacker.api.service
 
 import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
 import jetpacker.api.configuration.Endpoint
 import jetpacker.api.configuration.JetpackerProperties
 import jetpacker.api.configuration.Kit
 import jetpacker.api.configuration.Releases
+import jetpacker.api.configuration.Tab
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.scheduling.annotation.Scheduled
@@ -17,6 +19,7 @@ import java.util.concurrent.ExecutionException
  * Created by donny on 30/10/16.
  */
 @CompileStatic
+@Slf4j
 @Service
 class TaskService {
     private final JetpackerProperties jetpackerProperties
@@ -34,15 +37,27 @@ class TaskService {
     void updateCache() {
         try {
             getSdkManCandidates()
+            // TODO: clone the jetpacker properties items and assigned to cache.
         } catch (InterruptedException | ExecutionException e) {}
     }
 
     List<Kit> getSdkManCandidates() {
-        ResponseEntity<String> candidates = asyncRestTemplate.getForEntity(Endpoint.SdkMan.url, String.class).get()
+        ResponseEntity<String> rawCandidates = asyncRestTemplate.getForEntity(Endpoint.SdkMan.url, String.class).get()
 
-        candidates.body.split(",").collect { String candidate ->
-            ResponseEntity<String> versions = asyncRestTemplate.getForEntity("${Endpoint.SdkMan.url}/${candidate}", String.class).get()
-            new Kit(name: candidate, releases: new Releases())
+        List<Kit> kits = rawCandidates.body.split(",").collect { String candidate ->
+            ResponseEntity<String> rawVersions = asyncRestTemplate.getForEntity("${Endpoint.SdkMan.url}/${candidate}", String.class).get()
+            List<String> versions = Arrays.asList(rawVersions.body.split(",")).reverse()
+            new Kit(name: candidate,
+                    releases: new Releases(
+                            selectedVersion: ":${candidate}_version",
+                            defaultVersion: versions[0],
+                            versions: versions
+                    ),
+                    tab: new Tab(
+                            index: 1,
+                            label: 'SDKMAN!'
+                    )
+            )
         }
     }
 }
