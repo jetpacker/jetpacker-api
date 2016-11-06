de6dc4a2e3304ea6af6f4a5a0554d12362b51a68
forseti <slickwolf@gmail.com>
2016-11-06 13:53:30 +0800

    Add endpoint '/items' which returns available items for generator

--- src/main/groovy/jetpacker/api/controller/GeneratorController.groovy
+++ src/main/groovy/jetpacker/api/controller/GeneratorController.groovy
@@ -1,5 +1,10 @@
 package jetpacker.api.controller
 
+import jetpacker.api.cache.Jetpacker
+import jetpacker.api.service.CacheService
+import jetpacker.api.service.GeneratorService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 /**
@@ -7,4 +12,17 @@ import org.springframework.web.bind.annotation.RestController
  */
 @RestController
 class GeneratorController {
+    private final CacheService cacheService
+    private final GeneratorService generatorService
+
+    @Autowired
+    GeneratorController(CacheService cacheService, GeneratorService generatorService) {
+        this.cacheService = cacheService
+        this.generatorService = generatorService
+    }
+
+    @GetMapping("/items")
+    Jetpacker items() {
+        cacheService.items()
+    }
 }
