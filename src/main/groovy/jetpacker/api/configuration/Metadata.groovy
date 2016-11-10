25371f6e54c7b58597ed638149e1fc66b16b0074
forseti <slickwolf@gmail.com>
2016-11-10 09:12:21 +0800

    Ignore unknown properties on JSON bindingg

--- src/main/groovy/jetpacker/api/configuration/Metadata.groovy
+++ src/main/groovy/jetpacker/api/configuration/Metadata.groovy
@@ -1,11 +1,13 @@
 package jetpacker.api.configuration
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import groovy.transform.CompileStatic
 
 /**
  * Created by donny on 24/10/2016.
  */
 @CompileStatic
+@JsonIgnoreProperties(ignoreUnknown = true)
 class Metadata {
     String name
     String label
