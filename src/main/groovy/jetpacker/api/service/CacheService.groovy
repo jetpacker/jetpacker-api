b1cef82b06afb925952c33de8eb82085fe97187b
forseti <slickwolf@gmail.com>
2016-11-13 21:54:49 +0800

    Complete basic functionality to get releases.

--- src/main/groovy/jetpacker/api/service/CacheService.groovy
+++ src/main/groovy/jetpacker/api/service/CacheService.groovy
@@ -1,6 +1,5 @@
 package jetpacker.api.service
 
-import com.github.zafarkhaja.semver.Version
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import jetpacker.api.configuration.*
@@ -12,7 +11,6 @@ import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import org.springframework.web.client.AsyncRestTemplate
 
-import javax.xml.ws.Response
 import java.util.concurrent.ExecutionException
 
 /**
@@ -22,7 +20,6 @@ import java.util.concurrent.ExecutionException
 @Slf4j
 @Service
 class CacheService {
-
     private final JetpackerProperties jetpackerProperties
     private final AsyncRestTemplate asyncRestTemplate
 
@@ -43,43 +40,85 @@ class CacheService {
             jetpackerProperties.openjdk.extensions = getSdkManCandidates()
 
             [ jetpackerProperties.node, jetpackerProperties.guard ].each { Kit kit ->
-                if (kit.endpoint != null && kit.endpoint == Endpoint.GitHub) {
-                    String url = Endpoint.GitHub.url.replace("{namespace}", kit.namespace)
-                                                    .replace("{name}", kit.name)
-
-                    ResponseEntity<List<Metadata>> response = asyncRestTemplate.exchange(url, HttpMethod.GET, null,
-                                                                new ParameterizedTypeReference<List<Metadata>>() {}).get()
-
-                    kit.releases.versions = response.body.collect { Metadata metadata -> metadata.name }
-                    kit.releases.defaultVersion = kit.releases.versions[0]
-                }
-
-
-                Kit dependency = kit.dependency
-
-                if (dependency && dependency.endpoint != null && dependency.endpoint == Endpoint.GitHub) {
-                    String url = Endpoint.GitHub.url.replace("{namespace}", dependency.namespace)
-                                                    .replace("{name}", dependency.name)
-
-                    ResponseEntity<List<Metadata>> response = asyncRestTemplate.exchange(url, HttpMethod.GET, null,
-                            new ParameterizedTypeReference<List<Metadata>>() {}).get()
-
-                    List<String> versions = response.body.collect { Metadata metadata ->
-                        if (metadata.name =~ /^v?[0-9]+(\.|_)[0-9]+(\.|_)[0-9]+$/)
-                            metadata.name
-                    }
-
-                    versions.removeAll(Arrays.asList(null))
-                    dependency.releases.defaultVersion = versions[0]
-                }
+                updateReleases(kit)
             }
 
-        } catch (InterruptedException | ExecutionException e) {}
+            jetpackerProperties.databases.each { Container container ->
+                updateReleases(container)
+            }
+
+            jetpackerProperties.messageQueues.each { Container container ->
+                updateReleases(container)
+            }
+
+            jetpackerProperties.searchEngines.each { Container container ->
+                updateReleases(container)
+            }
+
+
+        } catch (InterruptedException|ExecutionException e) {}
     }
 
     @Scheduled(fixedRate = 30000L)
     private void updateCache() {
         // TODO: Update only the release versions
+    }
+
+    private void updateReleases(Application application, Boolean dependency = false) {
+        if (application instanceof Kit) {
+            Kit kit = (Kit) application
+
+            if (kit.dependency)
+                updateReleases(kit.dependency, true)
+        }
+
+        Endpoint endpoint = application.endpoint
+
+        if (endpoint && endpoint != Endpoint.SdkMan) {
+            String url = endpoint.url.replace("{name}", application.name)
+
+            if (application.namespace)
+                url = url.replace("{namespace}", application.namespace)
+
+            List<Metadata> metadataList = null
+
+            if (endpoint == Endpoint.GitHub) {
+                ResponseEntity<List<Metadata>> response = asyncRestTemplate.exchange(url, HttpMethod.GET, null,
+                                                            new ParameterizedTypeReference<List<Metadata>>() {}).get()
+                metadataList = response.body
+            }
+
+            if (endpoint == Endpoint.DockerHub) {
+                ResponseEntity<Docker> response = asyncRestTemplate.exchange(url, HttpMethod.GET, null,
+                                                    new ParameterizedTypeReference<Docker>() {}).get()
+                Docker docker = response.body
+                metadataList = docker.results
+
+                while (docker.next) {
+                    response =  asyncRestTemplate.exchange(docker.next, HttpMethod.GET, null,
+                                    new ParameterizedTypeReference<Docker>() {}).get()
+                    docker = response.body
+                    metadataList += docker.results
+                }
+
+            }
+
+            if (metadataList) {
+                List<String> versions = metadataList.collect { Metadata metadata ->
+                    String pattern = "^v?[0-9]+([\\._][0-9]+)*(${application.suffix})?\$"
+
+                    if (metadata.name =~ pattern)
+                        metadata.name
+                }
+
+                versions.removeAll(Arrays.asList(null))
+
+                if (!dependency)
+                    application.releases.versions = versions
+
+                application.releases.defaultVersion = versions[0]
+            }
+        }
     }
 
     private List<Kit> getSdkManCandidates() throws ExecutionException, InterruptedException {
