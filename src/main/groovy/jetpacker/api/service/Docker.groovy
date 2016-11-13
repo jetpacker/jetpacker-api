c321d80d3ee25b26f3808f6fa722f5bf3f094400
forseti <slickwolf@gmail.com>
2016-11-13 21:55:56 +0800

    Add docker wrapper to consume the Docker releases information

---
+++ src/main/groovy/jetpacker/api/service/Docker.groovy
@@ -0,0 +1,12 @@
+package jetpacker.api.service
+
+import jetpacker.api.configuration.Metadata
+
+/**
+ * Created by wolf on 13/11/16.
+ */
+class Docker {
+    String next
+    String previous
+    List<Metadata> results
+}
