cc0d34ce24af16dcab41635fce5d39e0fcd5bff8
forseti <slickwolf@gmail.com>
2016-11-23 15:59:32 +0800

    Move logic that retrieves releases from SDKMAN, DockerHub, and GitHub to PropertiesService and RepositoryService

--- src/main/groovy/jetpacker/api/controller/GeneratorController.groovy
+++ src/main/groovy/jetpacker/api/controller/GeneratorController.groovy
@@ -1,26 +1,11 @@
 package jetpacker.api.controller
 
-import jetpacker.api.configuration.Application
-import jetpacker.api.configuration.Container
-import jetpacker.api.configuration.Repository
 import jetpacker.api.configuration.JetpackerProperties
-import jetpacker.api.configuration.Kit
-import jetpacker.api.configuration.Metadata
-import jetpacker.api.configuration.Parameter
-import jetpacker.api.configuration.Version
-import jetpacker.api.service.DockerHub
 import jetpacker.api.service.GeneratorService
-import org.apache.maven.artifact.versioning.DefaultArtifactVersion
+import jetpacker.api.service.PropertiesService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.core.ParameterizedTypeReference
-import org.springframework.http.HttpMethod
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.client.AsyncRestTemplate
-
-import javax.annotation.PostConstruct
-import java.util.concurrent.ExecutionException
 
 /**
  * Created by donny on 30/10/16.
@@ -28,129 +13,17 @@ import java.util.concurrent.ExecutionException
 @RestController
 class GeneratorController {
     private final GeneratorService generatorService
-    private final JetpackerProperties jetpackerProperties
-    private final AsyncRestTemplate asyncRestTemplate
+    private final PropertiesService propertiesService
 
     @Autowired
     GeneratorController(GeneratorService generatorService,
-                        JetpackerProperties jetpackerProperties,
-                        AsyncRestTemplate asyncRestTemplate) {
+                        PropertiesService propertiesService) {
         this.generatorService = generatorService
-        this.jetpackerProperties = jetpackerProperties
-        this.asyncRestTemplate = asyncRestTemplate
+        this.propertiesService = propertiesService
     }
 
     @GetMapping("/load")
     JetpackerProperties load() {
-        jetpackerProperties
-    }
-
-    @PostConstruct
-    private void setUp() {
-        try {
-            jetpackerProperties.ubuntu.timezone.availableIds = TimeZone.availableIDs.collect { String id ->
-                if (!id.startsWith("SystemV"))
-                    return id
-            }
-
-            jetpackerProperties.ubuntu.timezone.availableIds.removeAll([ null ])
-
-            jetpackerProperties.openjdk.extensions = retrieveSdkManCandidates()
-
-            [ jetpackerProperties.node, jetpackerProperties.guard ].each { Kit kit ->
-                updateReleases(kit)
-            }
-
-            jetpackerProperties.databaseEngines.each { Container container ->
-                updateReleases(container)
-            }
-
-            jetpackerProperties.queueEngines.each { Container container ->
-                updateReleases(container)
-            }
-
-            jetpackerProperties.searchEngines.each { Container container ->
-                updateReleases(container)
-            }
-        } catch (InterruptedException|ExecutionException e) {}
-    }
-
-    private List<Kit> retrieveSdkManCandidates() throws ExecutionException, InterruptedException {
-        ResponseEntity<String> response = asyncRestTemplate.getForEntity(Repository.SdkMan.url, String.class).get()
-
-        response.body.split(",").collect { String candidate ->
-            ResponseEntity<String> rawVersions = asyncRestTemplate.getForEntity("${Repository.SdkMan.url}/${candidate}", String.class).get()
-            List<String> versions = Arrays.asList(rawVersions.body.split(",")).reverse()
-            versions.sort versionComparator
-
-            new Kit(name: candidate,
-                    label: candidate,
-                    version: new Version(
-                            releases: versions,
-                            name: "${candidate}_version",
-                            label: "Version"
-                    )
-            )
-        }
-    }
-
-    private void updateReleases(Application application, Boolean dependency = false) {
-        if (application instanceof Kit) {
-            Kit kit = (Kit) application
-
-            if (kit.dependency)
-                updateReleases(kit.dependency, true)
-        }
-
-        Repository endpoint = application.repository
-
-        if (endpoint && endpoint != Repository.SdkMan) {
-            String url = endpoint.url.replace("{name}", application.name)
-
-            if (application.namespace)
-                url = url.replace("{namespace}", application.namespace)
-
-            List<Metadata> metadataList = null
-
-            if (endpoint == Repository.GitHub) {
-                ResponseEntity<List<Metadata>> response = asyncRestTemplate.exchange(url, HttpMethod.GET, null,
-                        new ParameterizedTypeReference<List<Metadata>>() {}).get()
-                metadataList = response.body
-            }
-
-            if (endpoint == Repository.DockerHub) {
-                ResponseEntity<DockerHub> response = asyncRestTemplate.exchange(url, HttpMethod.GET, null,
-                        new ParameterizedTypeReference<DockerHub>() {}).get()
-                DockerHub dockerHub = response.body
-                metadataList = dockerHub.results
-
-                while (dockerHub.next) {
-                    response =  asyncRestTemplate.exchange(dockerHub.next, HttpMethod.GET, null,
-                            new ParameterizedTypeReference<DockerHub>() {}).get()
-                    dockerHub = response.body
-                    metadataList += dockerHub.results
-                }
-
-            }
-
-            if (metadataList) {
-                List<String> versions = metadataList.collect { Metadata metadata ->
-                    String pattern = "^v?[0-9]+([\\._][0-9]+)*(${application.suffix})?\$"
-
-                    if (metadata.name =~ pattern)
-                        metadata.name
-                }
-
-                versions.removeAll([ null ])
-                versions.sort versionComparator
-
-                if (!dependency)
-                    application.version.releases = versions
-            }
-        }
-    }
-
-    private def versionComparator = { String a, String b ->
-        new DefaultArtifactVersion(b) <=> new DefaultArtifactVersion(a)
+        propertiesService.load()
     }
 }
--- src/main/groovy/jetpacker/api/service/RepositoryService.groovy
+++ src/main/groovy/jetpacker/api/service/RepositoryService.groovy
@@ -1,18 +1,131 @@
 package jetpacker.api.service
 
+import jetpacker.api.common.VersionComparator
+import jetpacker.api.configuration.*
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.HttpMethod
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import org.springframework.web.client.AsyncRestTemplate
+
+import java.util.concurrent.ExecutionException
 
 /**
  * Created by donny on 22/11/16.
  */
 @Service
 class RepositoryService {
+    private final VersionComparator versionComparator = new VersionComparator()
+
     private final AsyncRestTemplate asyncRestTemplate
 
     @Autowired
     RepositoryService(AsyncRestTemplate asyncRestTemplate) {
         this.asyncRestTemplate = asyncRestTemplate
+    }
+
+    public void updateReleases(Application application) {
+        if (application instanceof Kit) {
+            Kit kit = (Kit) application
+
+            if (kit.dependency)
+                updateReleases(kit.dependency)
+        }
+
+        Repository repository = application.repository
+        Summary summary = (Summary) application
+
+        if (repository == Repository.GitHub)
+            application.version.releases = loadReleasesFromGitHub(repository, summary)
+
+        if (repository == Repository.DockerHub)
+            application.version.releases = loadReleasesFromDockerHub(repository, summary)
+
+    }
+
+    public List<Version> filterReleases(List<String> releases, String suffix) {
+        if (releases) {
+            releases = releases.collect { String release ->
+                String pattern = "^v?[0-9]+([\\._][0-9]+)*(${suffix})?\$"
+
+                if (release =~ pattern)
+                    release
+            }
+
+            releases.removeAll([ null ])
+            releases.sort versionComparator
+        }
+
+        releases
+    }
+
+    public String loadUrlFromRepository(Repository repository, Summary summary) {
+        String url = repository.url
+
+        if (repository && repository != Repository.SdkMan) {
+            String name = summary.name
+            String namespace = summary.namespace
+
+            url = url.replace("{name}", name)
+
+            if (namespace)
+                url = url.replace("{namespace}", namespace)
+        }
+
+        url
+    }
+
+    public List<String> loadReleasesFromGitHub(Repository repository, Summary summary) {
+        String url = loadUrlFromRepository(repository, summary)
+
+        ResponseEntity<List<Summary>> response = asyncRestTemplate.exchange(url, HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<Summary>>() {}).get()
+
+        List<Summary> summaries = response.body
+        List<String> releases = filterReleases(summaries*.name, summary.suffix)
+
+        releases
+    }
+
+    public List<String> loadReleasesFromDockerHub(Repository repository, Summary summary) {
+        String url = loadUrlFromRepository(repository, summary)
+
+        ResponseEntity<DockerHub> response = asyncRestTemplate.exchange(url, HttpMethod.GET, null,
+                new ParameterizedTypeReference<DockerHub>() {}).get()
+
+        DockerHub dockerHub = response.body
+        List<Summary> summaries = dockerHub.results
+
+        while (dockerHub.next) {
+            response =  asyncRestTemplate.exchange(dockerHub.next, HttpMethod.GET, null,
+                    new ParameterizedTypeReference<DockerHub>() {}).get()
+
+            dockerHub = response.body
+            summaries += dockerHub.results
+        }
+
+        List<String> releases = filterReleases(summaries*.name, summary.suffix)
+
+        releases
+    }
+
+    public List<Kit> loadCandidatesFromSdkMan() throws ExecutionException, InterruptedException {
+        ResponseEntity<String> response = asyncRestTemplate.getForEntity(Repository.SdkMan.url, String.class).get()
+
+        response.body.split(",").collect { String candidate ->
+            ResponseEntity<String> rawVersions = asyncRestTemplate.getForEntity("${Repository.SdkMan.url}/${candidate}", String.class).get()
+            List<String> versions = Arrays.asList(rawVersions.body.split(",")).reverse()
+            versions.sort versionComparator
+
+            new Kit(name: candidate,
+                    label: candidate,
+                    version: new Version(
+                            releases: versions,
+                            name: "${candidate}_version",
+                            label: "Version"
+                    )
+            )
+        }
     }
 }
