0eebb43c8553ee2cfa0b96bededd20fc967d84a7
forseti <slickwolf@gmail.com>
2016-11-26 23:54:34 +0800

    Add SpringFox Swagger

--- src/main/groovy/jetpacker/api/JetpackerApplication.groovy
+++ src/main/groovy/jetpacker/api/JetpackerApplication.groovy
@@ -4,9 +4,10 @@ import groovy.transform.CompileStatic
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
-@EnableScheduling
+@EnableSwagger2
 @CompileStatic
 class JetpackerApplication {
 	static void main(String[] args) {
