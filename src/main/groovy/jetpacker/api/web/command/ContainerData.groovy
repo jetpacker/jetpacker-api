71a8817bed9b6a7547f6a97f5e48a4287f959158
forseti <slickwolf@gmail.com>
2016-11-30 13:30:45 +0800

    Introduce initial Command Objects for Generator Controller

---
+++ src/main/groovy/jetpacker/api/web/command/ContainerData.groovy
@@ -0,0 +1,11 @@
+package jetpacker.api.web.command
+
+/**
+ * Created by donny on 30/11/16.
+ */
+class ContainerData {
+    String command
+    Map<String, String> volumes
+    Map<String, String> publishedPorts
+    Map<String, String> environmentVariables
+}
---
+++ src/main/groovy/jetpacker/api/web/command/KitData.groovy
@@ -0,0 +1,9 @@
+package jetpacker.api.web.command
+
+/**
+ * Created by donny on 30/11/16.
+ */
+class KitData {
+    String version
+    Map<String, String> extensions
+}
