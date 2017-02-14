215d6ba54650efe46160fbddd34b7eeb60873c5f
forseti <slickwolf@gmail.com>
2017-02-14 12:44:49 +0800

    Add Box to store available Vagrant boxes

---
+++ src/main/groovy/io/jetpacker/api/configuration/Box.groovy
@@ -0,0 +1,10 @@
+package io.jetpacker.api.configuration
+
+import groovy.transform.CompileStatic
+/**
+ * Created by donny on 14/2/17.
+ */
+@CompileStatic
+class Box extends Parameter {
+    Map<String, String> releases
+}
