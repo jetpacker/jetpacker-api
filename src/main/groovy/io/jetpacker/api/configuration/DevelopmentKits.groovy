bb0f609eb589777fce0a3386cc9b2b2e5efc29b9
forseti <slickwolf@gmail.com>
2017-02-14 12:40:54 +0800

    Change creator's name

--- src/main/groovy/io/jetpacker/api/configuration/DevelopmentKits.groovy
+++ src/main/groovy/io/jetpacker/api/configuration/DevelopmentKits.groovy
@@ -3,7 +3,7 @@ package io.jetpacker.api.configuration
 import groovy.transform.CompileStatic
 
 /**
- * Created by wolf on 24/1/17.
+ * Created by donny on 24/1/17.
  */
 @CompileStatic
 class DevelopmentKits {
