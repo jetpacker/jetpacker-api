fa53f47fa183e8a19fa93b7f0092d490cbfe5c1d
forseti <slickwolf@gmail.com>
2017-02-14 12:40:17 +0800

    Add creation time

--- src/main/groovy/io/jetpacker/api/JetpackerApplication.groovy
+++ src/main/groovy/io/jetpacker/api/JetpackerApplication.groovy
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
+/**
+ * Created by donny on 30/10/16.
+ */
 @SpringBootApplication
 @EnableSwagger2
 @CompileStatic
