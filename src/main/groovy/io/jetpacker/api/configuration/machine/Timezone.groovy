091a3e5ea695a6b34e3a229c3d5ce024cf4e9169
forseti <slickwolf@gmail.com>
2017-02-17 15:54:48 +0800

    Move TimeZone's ids retrieval to Timezone's constructor

--- src/main/groovy/io/jetpacker/api/configuration/machine/Timezone.groovy
+++ src/main/groovy/io/jetpacker/api/configuration/machine/Timezone.groovy
@@ -1,12 +1,25 @@
 package io.jetpacker.api.configuration.machine
 
 import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
 import io.jetpacker.api.configuration.Parameter
 
 /**
  * Created by donny on 15/11/16.
  */
+@Slf4j
 @CompileStatic
 class Timezone extends Parameter {
-    List<String> ids
+    final List<String> ids
+
+    Timezone() {
+        // TODO: TimeZone can be refactored for better testability
+        log.info "Loading timezones"
+        ids = TimeZone.availableIDs.collect { String id ->
+            if (!id.startsWith("SystemV"))
+                return id
+        }
+
+        ids.removeAll([ null ])
+    }
 }
