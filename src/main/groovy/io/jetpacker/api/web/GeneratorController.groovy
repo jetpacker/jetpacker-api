43fd5b98f8feed52246343e5b42bf9b9eee6950b
forseti <slickwolf@gmail.com>
2017-02-23 16:08:26 +0800

    Add initial Generator.generate endpoint

--- src/main/groovy/io/jetpacker/api/web/GeneratorController.groovy
+++ src/main/groovy/io/jetpacker/api/web/GeneratorController.groovy
@@ -1,16 +1,17 @@
 package io.jetpacker.api.web
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import groovy.util.logging.Slf4j
 import io.jetpacker.api.configuration.JetpackerProperties
 import io.jetpacker.api.core.GeneratorService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 /**
  * Created by donny on 30/10/16.
  */
+@Slf4j
 @CrossOrigin
 @RestController
 @RequestMapping("/generator")
@@ -25,5 +26,12 @@ class GeneratorController {
     @GetMapping
     JetpackerProperties load() {
         generatorService.load()
+    }
+
+    @PostMapping
+    void generate(@RequestBody Map<Object, Object> body) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        log.info "body: ${mapper.writeValueAsString(body)}";
     }
 }
