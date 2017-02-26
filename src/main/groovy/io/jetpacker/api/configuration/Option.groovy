ec0f73bac7f0ae92e74fabeb7df21df76cb2e65e
forseti <slickwolf@gmail.com>
2017-02-26 22:41:51 +0800

    Add Option.groovy

---
+++ src/main/groovy/io/jetpacker/api/configuration/Option.groovy
@@ -0,0 +1,12 @@
+package io.jetpacker.api.configuration
+
+import groovy.transform.CompileStatic
+
+/**
+ * Created by wolf on 26/2/17.
+ */
+@CompileStatic
+class Option {
+    String label
+    String value
+}
